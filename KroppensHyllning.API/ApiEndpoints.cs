0b2e73e18a05775b90254bb9514c3c5106069df9
David Malmström <syntex@outlook.com>
2023-09-25 22:17:17 +0200

    feat: basic crud

---
+++ KroppensHyllning.API/ApiEndpoints.cs
@@ -0,0 +1,21 @@
+namespace KroppenHyllning;
+
+public static class ApiEndpoints
+{
+    private const string ApiBase = "api";
+
+    public static class Campaigns
+    {
+        private const string Base = $"{ApiBase}/campaigns";
+
+        public const string Create = Base;
+
+        public const string Get = $"{Base}/{{id:guid}}";
+
+        public const string GetAll = Base;
+
+        public const string Update = $"{Base}/{{id:guid}}";
+
+        public const string Delete = $"{Base}/{{id:guid}}";
+    }
+}
